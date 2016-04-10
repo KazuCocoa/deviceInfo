08f131f35d65f82ac596f635feeab22b01fb23ba
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-04-11 00:46:31 +0900

    initial commit

---
+++ deviceInfo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  deviceInfo
+//
+//  Created by 松尾和昭 on 10/04/2016.
+//  Copyright © 2016 kazucocoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+
+
+}
+
